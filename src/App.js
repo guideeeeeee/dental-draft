e697730457c630b9d8095b32c83505b56bfeb580
guideeeeeee <pratom5108@gmail.com>
2023-09-21 17:02:00 +0700

    gpt

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="createApp"> 
+      <h1>CREATE NEW ACCOUNT</h1>
+      <div className="form1">
+        <form className="formleft">
+          <label>Firstname:</label>
+          <input type="text" />
+          <label>Lastname:</label>
+          <input type="text" />
+          <label>Tel:</label>
+          <input type="text" />
+          <label>ID line:</label>
+          <input type="text" />
+          <label>Email:</label>
+          <input type="email" />
+          <label>Password:</label>
+          <input type="password" />
+          <label>ID Card:</label>
+          <input type="text" />
+          <label>religion:</label>
+          <input type="text" />
+          <label>Nationality:</label>
+          <input type="text" />
+          <label>Race:</label>
+          <input type="text" />
+        </form>
+        <form className="formright">
+          <label>Ouccupation:</label>
+          <input type="text" />
+          <label>Date of Birth:</label>
+          <input type="text" />
+          <label>Weight:</label>
+          <input type="number" />
+          <label>Height:</label>
+          <input type="number" />
+          <label>Address</label>
+          <input type="text" />
+          <label>Allergy History:</label>
+          <input type="text" />
+        </form>
+      </div>
+      <button className="btn">Sign up</button>
     </div>
   );
 }
