b06600808f0eb3203ae01b04a5e3d913494fba25
guideeeeeee <pratom5108@gmail.com>
2023-10-10 12:41:24 +0700

    add confirmpass

--- src/App.js
+++ src/App.js
@@ -5,66 +5,111 @@ function App() {
       <h1>CREATE NEW ACCOUNT</h1>
       <div className="form1">
         <form className="formleft">
-          <span>
-            <label>Firstname:</label>
-            <label>Lastname:</label>
-          </span>
-          <span>
-          <input type="text" />
-          <input type="text" />
-          </span>
-          <span>
-          <label>Tel:</label>
-          <label>ID line:</label>
-          </span>
-          <span>
-          <input type="text" />
-          <input type="text" />
-          </span>
-          <label>Email:</label>
-          <input type="email" />
-          <label>Password:</label>
-          <input type="password" />
-          <label>ID Card:</label>
-          <input type="text" />
-          <span>
-          <label>religion:</label>
-          <label>Nationality:</label>
-          <label>Race:</label>
-          </span>
-          <span>
-          <input type="text" />
-          <input type="text" />
-          <input type="text" />
-          </span>
+          {/* name */}
+          <div className="column">
+            <div className="column-name-text">
+              <label>Firstname:</label>
+              <input type="text" />
+            </div>
+            <div className="column-name-text">
+              <label>Lastname:</label>
+              <input type="text" />
+            </div>
+          </div>
+
+          {/* tel idLine */}
+          <div className="column">
+            <div className="column-name-text">
+              <label>Tel:</label>
+              <input type="text" />
+            </div>
+            <div className="column-name-text">
+              <label>ID line:</label>
+              <input type="text" />
+            </div>
+          </div>
+
+          {/* email */}
+          <div className="name-text">
+            <label>Email:</label>
+            <input type="email" />
+          </div>
+
+          {/* password */}
+          <div className="name-text">
+            <label>Password:</label>
+            <input type="password" />
+          </div>
+          {/* confirm password */}
+          <div className="name-text">
+            <label>Confirm password:</label>
+            <input type="password" />
+          </div>
+          {/* ID card */}
+          <div className="name-text">
+            <label>ID Card:</label>
+            <input type="text" />
+          </div>
         </form>
         <form className="formright">
-          <label>Ouccupation:</label>
-          <input type="text" />
-          <label>Date of Birth:</label>
-          <input type="text" />
-          <span>
-          <label>Weight:</label>
-          <label>Height:</label>
-          </span>
-          <span>
-          <input type="number" />
-          <input type="number" />
-          </span>
-          <label>Address:</label>
-          <input type="text" />
+          {/* religion Nation */}
+          <div className="column">
+            <div className="column-name-text">
+              <label>Religion:</label>
+              <input type="text" />
+            </div>
+            <div className="column-name-text">
+              <label>Nationality:</label>
+              <input type="text" />
+            </div>
+          </div>
+          {/* dob race */}
+          <div className="column">
+            <div className="column-name-text">
+              <label>Race:</label>
+              <input type="text" />
+            </div>
+            <div className="column-name-text">
+              <label>Date of Birth:</label>
+              <input type="text" />
+            </div>
+          </div>
+          {/* ouccupation */}
+          <div className="name-text">
+            <label>Ouccupation:</label>
+            <input type="text" />
+          </div>
+          {/* weight height */}
+          <div className="column">
+            <div className="column-name-text">
+              <label>Weight:</label>
+              <input type="number" />
+            </div>
+            <div className="column-name-text">
+              <label>Height:</label>
+              <input type="number" />
+            </div>
+          </div>
+          <div className="name-text">
+            <label>Address:</label>
+            <input type="text" />
+          </div>
+          <div className="name-text">
           <label>Allergy History:</label>
           <input type="text" />
+          </div>
+          {/*
+          
+           */}
         </form>
       </div>
       <button className="btn">Sign up</button>
       <ul>
         <span className="bottomClick">
-        <li>LOG IN</li>
-        <li>FORGOT PASSWORD?</li>
-        <li>HOME</li>
+          <li>LOG IN</li>
+          <li>FORGOT PASSWORD?</li>
+          <li>HOME</li>
         </span>
-        
       </ul>
     </div>
   );
